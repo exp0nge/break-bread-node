74da988a1a66d433d9a34bfba7583e734d92e9a2
exp0nge <mdislamwork@gmail.com>
2015-12-24 17:22:41 -0500

    Added add restaurant form to add to restaurant collection and user collection

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-    res.render('restaurant-index', {
-        title: 'Restaurant Dashboard',
-        user: req.session.user
+    req.db.get('owner').findById(req.session.user._id)
+        .success(function(doc){
+            res.render('restaurant-index', {
+            title: 'Restaurant Dashboard',
+            user: req.session.user,
+            restaurants: doc.restaurant
+            });
+        })
+        .error(function(err){
+            console.log(err);
         })
 });
 
@@ -13,6 +20,24 @@ router.get('/add/restaurant', function(req, res, next){
 });
 
 router.post('/add/restaurant', function(req, res, next){
+    var restaurant = {
+        "name": req.body.name,
+        "address": req.body.address,
+        "city": req.body.city,
+        "zip": req.body.zip,
+        "items": []
+    }
+    console.log(restaurant);
+    req.db.get('restaurant').insert(restaurant);
+    req.db.get('owner').update({ 
+        _id: req.session.user._id },
+        { $push: { restaurant: restaurant } })
+        .success(function(doc){
+            res.redirect('/restaurant');
+        })
+        .error(function(err){
+            console.log(err);
+        });
     
 });
 
